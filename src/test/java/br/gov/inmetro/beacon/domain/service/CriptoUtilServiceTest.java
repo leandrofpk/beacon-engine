52fe2cb11cc66df42630de972ae1ee2e5c8bd3f8
Leandro Pinho <leandro.fpk@gmail.com>
2019-05-14 09:33:52 -0300

    Test RSA example

--- src/test/java/br/gov/inmetro/beacon/domain/service/CriptoUtilServiceTest.java
+++ src/test/java/br/gov/inmetro/beacon/domain/service/CriptoUtilServiceTest.java
@@ -2,8 +2,15 @@ package br.gov.inmetro.beacon.domain.service;
 
 import org.junit.Test;
 
+import java.io.*;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 import static br.gov.inmetro.beacon.domain.service.CriptoUtilService.sign;
 import static org.junit.Assert.assertEquals;
@@ -36,5 +43,46 @@ public class CriptoUtilServiceTest {
         boolean isCorrect = CriptoUtilService.verify(originalString, signature, pair.getPublic());
         System.out.println("Signature correct: " + isCorrect);
     }
+
+    @Test
+    public void testeFile() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException {
+
+        final String PRIVATE_KEY="/home/user/private.der";
+        final String PUBLIC_KEY="/home/user/public.der";
+
+
+        //get the private key
+        File file = new File(PRIVATE_KEY);
+        FileInputStream fis = new FileInputStream(file);
+        DataInputStream dis = new DataInputStream(fis);
+
+        byte[] keyBytes = new byte[(int) file.length()];
+        dis.readFully(keyBytes);
+        dis.close();
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        RSAPrivateKey privKey = (RSAPrivateKey) kf.generatePrivate(spec);
+        System.out.println("Exponent :" + privKey.getPrivateExponent());
+        System.out.println("Modulus" + privKey.getModulus());
+
+        //get the public key
+        File file1 = new File(PUBLIC_KEY);
+        FileInputStream fis1 = new FileInputStream(file1);
+        DataInputStream dis1 = new DataInputStream(fis1);
+        byte[] keyBytes1 = new byte[(int) file1.length()];
+        dis1.readFully(keyBytes1);
+        dis1.close();
+
+        X509EncodedKeySpec spec1 = new X509EncodedKeySpec(keyBytes1);
+        KeyFactory kf1 = KeyFactory.getInstance("RSA");
+        RSAPublicKey pubKey = (RSAPublicKey) kf1.generatePublic(spec1);
+
+        System.out.println("Exponent :" + pubKey.getPublicExponent());
+        System.out.println("Modulus" + pubKey.getModulus());
+
+    }
+
+
 
 }
